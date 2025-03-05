1e39cb50eb460a66a6838be25cac9ac467c31fc3
Tiago Bazzi Zancanella <tiagozancanella17@gmail.com>
2025-03-05 18:57:22 -0300

    commit para acionar ao  vercel

--- script/pagar.js
+++ script/pagar.js
@@ -1,3 +1,35 @@
+// const url = new URL(window.location.href);
+// const params = new URLSearchParams(url.search);
+// const pagamentoFinalizar = params.get("id");
+// const urlAPI = "https://public.franciscosensaulas.com"
+
+
+
+
+
+
+
+// async function FinalizaPagamento(id) {
+//     let url = `${urlAPI}/api/v1/trabalho/pagamentos/${id}`;
+//     console.log(url);
+
+//     const resposta = await fetch(url, {method: "DELETE"});
+//     if(resposta.ok == false){
+//         alert("Não foi possível cancelar");
+//         return;
+//     }
+
+//     Swal.fire({
+//         title: "Cancelado!",
+//         text: "Produto cancelado com sucesso!",
+//         icon: "success"
+//     });
+
+//     consultarPedidos();  
+// }
+
+
+
 async function atribuirCliqueBotoesPagar() {
     let botoesPagar = document.getElementsByClassName("botaoPagar" );
 
@@ -8,11 +40,13 @@ async function atribuirCliqueBotoesPagar() {
 
 }
 async function pagarPedido() {
-    console.log("Pagamento processado com sucesso!");
-    
-    alert("Seu pedido foi pago com sucesso!");
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Pagamento realizado",
+        showConfirmButton: false,
+        timer: 1500
+      });
     
 }
 
-
-alert("chegou aqui")
